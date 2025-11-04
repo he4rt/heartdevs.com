c62a26ed8ae29ecedb61aa09282c24f23f53a7bd
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2025-11-04 15:23:00 -0300

    feat(admin): admin module

---
+++ app-modules/admin/src/Providers/AdminServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace He4rt\Admin\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+	public function register(): void
+	{
+	}
+	
+	public function boot(): void
+	{
+	}
+}
