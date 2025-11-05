8ecb077e4b60305b75342c9a3175bf63c424d78d
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2025-11-04 21:42:02 -0300

    feat(test): add UserResourceTest for user management functionality

---
+++ app-modules/user/tests/Feature/UserResourceTest.php
@@ -0,0 +1,102 @@
+<?php
+
+use Filament\Actions\DeleteAction;
+use Filament\Facades\Filament;
+use He4rt\User\Filament\Admin\Resources\Users\Pages\CreateUser;
+use He4rt\User\Filament\Admin\Resources\Users\Pages\EditUser;
+use He4rt\User\Filament\Admin\Resources\Users\Pages\ListUsers;
+use He4rt\User\Filament\Admin\Resources\Users\UserResource;
+use He4rt\User\Models\User;
+
+beforeEach(function () {
+    $user = User::factory()->create();
+    Filament::setCurrentPanel('admin');
+    $this->actingAs($user);
+});
+
+it('can register resources', function (): void {
+    expect(Filament::getResources())
+        ->toContain(UserResource::class);
+});
+
+it('renders the list of users', function () {
+    $users = User::factory()->count(5)->create();
+
+    $this->livewire(ListUsers::class)
+        ->assertOk()
+        ->assertCanSeeTableRecords($users);
+});
+
+it('can load the page', function () {
+    $this->livewire(CreateUser::class)
+        ->assertOk();
+});
+
+it('can create a user', function () {
+    $this->livewire(CreateUser::class)
+        ->fillForm([
+            'username' => 'newuser',
+            'email' => 'emailmaneiro@example.com',
+            'password' => 'password',
+        ])
+        ->call('create')
+        ->assertNotified()
+        ->assertRedirect();
+
+    $this->assertDatabaseHas(User::class, [
+        'username' => 'newuser',
+        'email' => 'emailmaneiro@example.com',
+    ]);
+});
+
+
+it('can load the page edit page', function () {
+    $user = User::factory()->create();
+
+    $this->livewire(EditUser::class, [
+        'record' => $user->id,
+    ])
+        ->assertOk()
+        ->assertSchemaStateSet([
+            'username' => $user->username,
+            'email' => $user->email,
+        ]);
+});
+
+
+it('can update a user', function () {
+    $user = User::factory()->create();
+
+    $newUserData = User::factory()->make();
+
+    $this->livewire(EditUser::class, [
+        'record' => $user->id,
+    ])
+        ->fillForm([
+            'username' => $newUserData->username,
+            'email' => $newUserData->email,
+        ])
+        ->call('save')
+        ->assertNotified();
+
+    $this->assertDatabaseHas(User::class, [
+        'id' => $user->id,
+        'username' => $newUserData->username,
+        'email' => $newUserData->email,
+    ]);
+});
+
+it('can delete a user', function () {
+    $user = User::factory()->create();
+
+    $this->livewire(EditUser::class, [
+        'record' => $user->id,
+    ])
+        ->callAction(DeleteAction::class)
+        ->assertNotified()
+        ->assertRedirect();
+
+    $this->assertDatabaseMissing($user);
+});
+
+
