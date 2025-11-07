605efecf949dab77a75bf1200fbf7e7e3ffbd04d
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2025-11-06 21:06:59 -0300

    feat(message): create message plugin and add in messageProvider

--- app-modules/meeting/src/Providers/MeetingServiceProvider.php
+++ app-modules/meeting/src/Providers/MeetingServiceProvider.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace He4rt\Meeting\Providers;
 
+use App\Enums\FilamentPanel;
+use Filament\Panel;
 use He4rt\Meeting\Contracts\MeetingRepository;
 use He4rt\Meeting\Contracts\MeetingTypeRepository;
 use He4rt\Meeting\Repositories\MeetingEloquentRepository;
 use He4rt\Meeting\Repositories\MeetingTypeEloquentRepository;
+use He4rt\Message\Plugins\AdminMessagePanelPlugin;
 use Illuminate\Support\ServiceProvider;
 
 class MeetingServiceProvider extends ServiceProvider
@@ -16,6 +19,13 @@ class MeetingServiceProvider extends ServiceProvider
     {
         $this->app->bind(MeetingRepository::class, MeetingEloquentRepository::class);
         $this->app->bind(MeetingTypeRepository::class, MeetingTypeEloquentRepository::class);
+
+        Panel::configureUsing(function (Panel $panel): void {
+            match ($panel->currentPanel()) {
+                FilamentPanel::Admin => $panel->plugin(new AdminMessagePanelPlugin()),
+                default => null,
+            };
+        });
     }
 
     public function boot(): void {}
