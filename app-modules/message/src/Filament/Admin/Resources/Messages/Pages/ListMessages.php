9ec610804dab504b26fd9adf909bf8a9146ea2ba
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2025-11-06 21:05:49 -0300

    feat(message): create CRUD message resource

---
+++ app-modules/message/src/Filament/Admin/Resources/Messages/Pages/ListMessages.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace He4rt\Message\Filament\Admin\Resources\Messages\Pages;
+
+use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ListRecords;
+use He4rt\Message\Filament\Admin\Resources\Messages\MessageResource;
+
+class ListMessages extends ListRecords
+{
+    protected static string $resource = MessageResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
+}
