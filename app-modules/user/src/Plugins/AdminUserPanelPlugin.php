3a09bedade6363ce7099e3befd8d9a9c2e6a11b1
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2025-11-06 21:34:37 -0300

    style: pint and rector

--- app-modules/user/src/Plugins/AdminUserPanelPlugin.php
+++ app-modules/user/src/Plugins/AdminUserPanelPlugin.php
@@ -7,7 +7,6 @@ namespace He4rt\User\Plugins;
 use App\Enums\FilamentPanel;
 use Filament\Contracts\Plugin;
 use Filament\Panel;
-use He4rt\Tenant\Filament\Admin\Resources\Tenants\TenantResource;
 use He4rt\User\Filament\Admin\Resources\Users\UserResource;
 
 class AdminUserPanelPlugin implements Plugin
@@ -20,7 +19,7 @@ class AdminUserPanelPlugin implements Plugin
     public function register(Panel $panel): void
     {
         $panel->resources([
-            UserResource::class
+            UserResource::class,
         ]);
     }
 
