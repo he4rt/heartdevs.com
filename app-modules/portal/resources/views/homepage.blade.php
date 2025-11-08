f1a82a19a124c2f3b7490d1ee264baf745a91368
kaster <diogokaster@gmail.com>
2025-11-08 17:06:57 -0300

    feat: enhance CSS theme management with variables and gradient background adjustments

--- app-modules/portal/resources/views/homepage.blade.php
+++ app-modules/portal/resources/views/homepage.blade.php
@@ -1,3 +1,1 @@
-<x-filament-panels::page full-height="true">
-    <section class="bg-primary-pink mx-auto mb-28 flex w-full flex-col items-center justify-center sm:mb-44"></section>
-</x-filament-panels::page>
+<x-filament-panels::page full-height="true"></x-filament-panels::page>
