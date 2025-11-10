62c0fbfd4993d96c5e3809e3ce49af58701c6789
kaster <diogokaster@gmail.com>
2025-11-10 08:33:22 -0300

    feat: add reusable button component with styles and integrate into hero section

--- app-modules/portal/resources/views/components/sections/hero.blade.php
+++ app-modules/portal/resources/views/components/sections/hero.blade.php
@@ -3,11 +3,23 @@
         <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-12">
             <div>
                 <x-portal::headline size="2xl" :keywords="['potencial']">
-                    <x-slot:badge>Comunidade Open Source</x-slot>
+                    <x-slot:badge>
+                        <x-filament::icon icon="heroicon-o-book-open" class="h-5 w-5" />
+                        Comunidade Open Source
+                    </x-slot>
+
                     <x-slot:title>Desenvolva seu potencial na comunidade</x-slot>
+
                     <x-slot:description>
                         Uma comunidade de desenvolvedores dedicada a ajudar iniciantes a se tornarem profissionais
                         através de projetos, mentorias e networking.
+                    </x-slot>
+                    <x-slot:actions>
+                        <x-portal::button icon="heroicon-s-chevron-right">Começar agora</x-portal::button>
+
+                        <x-portal::button icon="heroicon-s-chevron-right" variant="outline">
+                            Explorar projetos
+                        </x-portal::button>
                     </x-slot>
                 </x-portal::headline>
             </div>
