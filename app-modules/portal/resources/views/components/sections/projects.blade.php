cf8004594a2bf47ff9b3d1e32b7349da916c9a3f
kaster <diogokaster@gmail.com>
2025-11-10 14:56:56 -0300

    feat: add reusable tag component with styles and integrate into projects section cards

--- app-modules/portal/resources/views/components/sections/projects.blade.php
+++ app-modules/portal/resources/views/components/sections/projects.blade.php
@@ -13,10 +13,46 @@
             </x-slot>
         </x-portal::headline>
 
-        <div class="grid w-full grid-cols-1 items-center justify-center sm:grid-cols-3">
-            <p class="text-center">card 1</p>
-            <p class="text-center">card 2</p>
-            <p class="text-center">card 3</p>
+        <div class="grid w-full grid-cols-1 items-center justify-center gap-8 sm:grid-cols-3 lg:gap-16">
+            <x-portal::card>
+                <x-slot:title>4Noobs</x-slot>
+                <x-slot:description>Repositórios de conteúdos sobre diversas tecnologias.</x-slot>
+                <x-slot:tags>
+                    <x-portal::tag>Documentação</x-portal::tag>
+                    <x-portal::tag>Documentação</x-portal::tag>
+                </x-slot>
+                <x-slot:actions>
+                    <x-portal::button block>Ver projeto</x-portal::button>
+                </x-slot>
+            </x-portal::card>
+
+            <x-portal::card>
+                <x-slot:title>He4rtLabs</x-slot>
+                <x-slot:description>
+                    Projetos práticos desenvolvidos pela comunidade para aprendizado.
+                </x-slot>
+                <x-slot:tags>
+                    <x-portal::tag>Documentação</x-portal::tag>
+                    <x-portal::tag>Documentação</x-portal::tag>
+                </x-slot>
+                <x-slot:actions>
+                    <x-portal::button block>Ver projeto</x-portal::button>
+                </x-slot>
+            </x-portal::card>
+
+            <x-portal::card>
+                <x-slot:title>Desafios de Código</x-slot>
+                <x-slot:description>
+                    Desafios semanais para praticar suas habilidades de programação.
+                </x-slot>
+                <x-slot:tags>
+                    <x-portal::tag>Documentação</x-portal::tag>
+                    <x-portal::tag>Documentação</x-portal::tag>
+                </x-slot>
+                <x-slot:actions>
+                    <x-portal::button block>Ver projeto</x-portal::button>
+                </x-slot>
+            </x-portal::card>
         </div>
     </div>
 </section>
