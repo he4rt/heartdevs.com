9b3fb1c37412cc902758636d77f1c36d349f5565
kaster <diogokaster@gmail.com>
2025-11-10 10:24:27 -0300

    feat: add testimonials and articles sections with headline components and responsive grid layout

--- app-modules/portal/resources/views/components/sections/articles.blade.php
+++ app-modules/portal/resources/views/components/sections/articles.blade.php
@@ -1,1 +1,25 @@
-<section class="hp-section"></section>
+<section class="hp-section">
+    <div class="hp-container">
+        <x-portal::headline align="center" size="md" :keywords="['fodas']">
+            <x-slot:badge>
+                <x-filament::icon icon="heroicon-o-document-text" class="h-5 w-5" />
+                Nossos artigos
+            </x-slot>
+
+            <x-slot:title>Artigos fodas da comunidade</x-slot>
+
+            <x-slot:description>
+                Contribua com projetos open source e desenvolva habilidades reais enquanto constrói seu portfólio
+            </x-slot>
+        </x-portal::headline>
+
+        <div class="grid w-full grid-cols-1 items-center justify-center sm:grid-cols-3">
+            <p class="text-center">card 1</p>
+            <p class="text-center">card 2</p>
+            <p class="text-center">card 3</p>
+            <p class="text-center">card 4</p>
+            <p class="text-center">card 5</p>
+            <p class="text-center">card 6</p>
+        </div>
+    </div>
+</section>
--- app-modules/portal/resources/views/components/sections/testimonials.blade.php
+++ app-modules/portal/resources/views/components/sections/testimonials.blade.php
@@ -1,1 +1,22 @@
-<section class="hp-section"></section>
+<section class="hp-section">
+    <div class="hp-container">
+        <x-portal::headline align="center" size="md" :keywords="['membros']">
+            <x-slot:badge>
+                <x-filament::icon icon="heroicon-o-chat-bubble-oval-left-ellipsis" class="h-5 w-5" />
+                Depoimentos
+            </x-slot>
+
+            <x-slot:title>O que dizem nossos membros</x-slot>
+
+            <x-slot:description>
+                Histórias reais de desenvolvedores que transformaram suas carreiras através da nossa comunidade.
+            </x-slot>
+        </x-portal::headline>
+
+        <div class="grid w-full grid-cols-1 items-center justify-center sm:grid-cols-3">
+            <p class="text-center">card 1</p>
+            <p class="text-center">card 2</p>
+            <p class="text-center">card 3</p>
+        </div>
+    </div>
+</section>
