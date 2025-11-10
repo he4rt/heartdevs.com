ea86126d2a2ba7cdee562a383a9cf7f92b6345d9
kaster <diogokaster@gmail.com>
2025-11-10 07:44:45 -0300

    feat: implement reusable headline component with responsive styles and integrate into hero section

--- app-modules/portal/resources/views/components/sections/hero.blade.php
+++ app-modules/portal/resources/views/components/sections/hero.blade.php
@@ -1,6 +1,16 @@
 <section class="hp-section">
-    <div class="hp-container grid grid-cols-1 lg:grid-cols-2">
-        <div>headline</div>
-        <div>code block</div>
+    <div class="hp-container">
+        <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-12">
+            <div>
+                <x-portal::headline size="2xl" :keywords="['potencial']">
+                    <x-slot:title>Desenvolva seu potencial na comunidade</x-slot>
+                    <x-slot:description>
+                        Uma comunidade de desenvolvedores dedicada a ajudar iniciantes a se tornarem profissionais
+                        através de projetos, mentorias e networking.
+                    </x-slot>
+                </x-portal::headline>
+            </div>
+            <div>Image</div>
+        </div>
     </div>
 </section>
