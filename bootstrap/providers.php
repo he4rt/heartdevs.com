ffea933a899be21a3e3d5280715ea72c9eb75453
kaster <diogokaster@gmail.com>
2025-11-12 17:05:51 -0300

    fix: Reorder FilamentServiceProvider import in providers.php.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 use App\Providers\AppServiceProvider;
 use App\Providers\EventServiceProvider;
-use App\Providers\FilamentServiceProvider;
 use App\Providers\Filament\AdminPanelProvider;
 use App\Providers\Filament\EventPanelProvider;
 use App\Providers\Filament\GuestPanelProvider;
 use App\Providers\Filament\PartnerPanelProvider;
 use App\Providers\Filament\UserPanelProvider;
+use App\Providers\FilamentServiceProvider;
 use App\Providers\RouteServiceProvider;
 
 return [
