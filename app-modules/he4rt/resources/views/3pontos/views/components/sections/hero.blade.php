bd2c18729a1bf15f2b57c90ab56ae9fa48a81875
kaster <diogokaster@gmail.com>
2025-11-16 21:24:06 -0300

    feat: update headline component styles and enhance hero section content

--- app-modules/he4rt/resources/views/3pontos/views/components/sections/hero.blade.php
+++ app-modules/he4rt/resources/views/3pontos/views/components/sections/hero.blade.php
@@ -1,5 +1,13 @@
-<section>
+<section class="pt-10">
     <x-he4rt::headline>
-        <x-slot:title>Teste</x-slot>
+        <x-slot:badge>Section name</x-slot>
+        <x-slot:title>Participe do primeiro evento presencial da 3Pontos</x-slot>
+        <x-slot:description>
+            O Meetup da He4rt é um evento que reúne os nosso membros presencialmente e virtualmente pra trocarmos
+            conhecimentos, fazer networking e unir cada vez mais nossa comunidade.
+        </x-slot>
+        <x-slot:actions>
+            <x-he4rt::button>Button</x-he4rt::button>
+        </x-slot>
     </x-he4rt::headline>
 </section>
