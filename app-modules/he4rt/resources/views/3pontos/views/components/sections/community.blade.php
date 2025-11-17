069d06c5553183ec7475cfe6f40764d6835ad513
kaster <diogokaster@gmail.com>
2025-11-17 10:34:10 -0300

    feat: add community section to 3pontos homepage

---
+++ app-modules/he4rt/resources/views/3pontos/views/components/sections/community.blade.php
@@ -0,0 +1,25 @@
+<section class="hp-section">
+    <div class="hp-container grid grid-cols-1 items-start gap-x-12 lg:grid-cols-[1fr_4fr]">
+        <div class="mb-4 flex items-center justify-center sm:justify-start">
+            <x-3pontos::section-title size="lg">Comunidade 3 pontos</x-3pontos::section-title>
+        </div>
+
+        <div class="flex flex-col gap-8">
+            <div>
+                <x-he4rt::headline>
+                    <x-slot:title>Comunidade 3 Pontos: Seu hub de tecnologia</x-slot>
+                    <x-slot:description>
+                        Onde o networking é real, o aprendizado é contínuo e os desafios são inspiradores. Junte-se a
+                        centenas de profissionais de áreas estratégias e capacitados que estão construindo o futuro num
+                        ambiente qualificado e propício para que o melhor aconteça.
+                    </x-slot>
+                    <x-slot:actions>
+                        <x-he4rt::button>Button</x-he4rt::button>
+                    </x-slot>
+                </x-he4rt::headline>
+            </div>
+        </div>
+
+        <div>cards</div>
+    </div>
+</section>
--- app-modules/he4rt/resources/views/3pontos/views/homepage.blade.php
+++ app-modules/he4rt/resources/views/3pontos/views/homepage.blade.php
@@ -1,4 +1,5 @@
 <x-filament-panels::page full-height="true">
     <x-3pontos::sections.hero />
     <x-3pontos::sections.social-action />
+    <x-3pontos::sections.community />
 </x-filament-panels::page>
