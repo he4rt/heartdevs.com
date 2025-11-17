383d84114fdc19dcf503e5a108352c6483c1f412
kaster <diogokaster@gmail.com>
2025-11-17 14:00:25 -0300

    feat: expand community section with detailed cards and enhance card styling in 3pontos theme

--- app-modules/he4rt/resources/views/3pontos/views/components/sections/community.blade.php
+++ app-modules/he4rt/resources/views/3pontos/views/components/sections/community.blade.php
@@ -5,21 +5,55 @@
         </div>
 
         <div class="flex flex-col gap-8">
-            <div>
-                <x-he4rt::headline>
-                    <x-slot:title>Comunidade 3 Pontos: Seu hub de tecnologia</x-slot>
-                    <x-slot:description>
-                        Onde o networking é real, o aprendizado é contínuo e os desafios são inspiradores. Junte-se a
-                        centenas de profissionais de áreas estratégias e capacitados que estão construindo o futuro num
-                        ambiente qualificado e propício para que o melhor aconteça.
-                    </x-slot>
-                    <x-slot:actions>
-                        <x-he4rt::button>Button</x-he4rt::button>
-                    </x-slot>
-                </x-he4rt::headline>
-            </div>
+            <x-he4rt::headline>
+                <x-slot:title>Comunidade 3 Pontos: Seu hub de tecnologia</x-slot>
+                <x-slot:description>
+                    Onde o networking é real, o aprendizado é contínuo e os desafios são inspiradores. Junte-se a
+                    centenas de profissionais de áreas estratégias e capacitados que estão construindo o futuro num
+                    ambiente qualificado e propício para que o melhor aconteça.
+                </x-slot>
+                <x-slot:actions>
+                    <x-he4rt::button>Button</x-he4rt::button>
+                </x-slot>
+            </x-he4rt::headline>
         </div>
 
-        <div>cards</div>
+        <div class="col-span-2 grid grid-cols-8 gap-4">
+            <x-he4rt::card class="col-span-5">
+                <x-slot:title>Participe de Desafios que Importam</x-slot>
+                <x-slot:description>
+                    Esqueça exercícios de código genéricos. Na comunidade 3 Pontos, você trabalha em projetos reais
+                    propostos por empresas aceleradas. Eventos enriquecedores, desafios técnicos e colaborações que
+                    constroem seu portfólio enquanto resolvem problemas do mercado. Sua visão faz a diferença.
+                </x-slot>
+            </x-he4rt::card>
+
+            <x-he4rt::card class="col-span-3">
+                <x-slot:title>Networking com Líderes e Pares</x-slot>
+                <x-slot:description>
+                    Aqui você não é apenas um número. Você é parte de uma comunidade onde líderes de mercado, founders e
+                    outros profissionais estão acessíveis. Faça conexões genuínas, encontre mentores, colabore em
+                    projetos e expanda sua rede de forma orgânica e significativa.
+                </x-slot>
+            </x-he4rt::card>
+
+            <x-he4rt::card class="col-span-3">
+                <x-slot:title>Educação Que Acompanha o Mercado</x-slot>
+                <x-slot:description>
+                    O mercado de tecnologia evolui a cada dia. Nós também. Acesso a webinars, workshops e discussões
+                    sobre as últimas tendências: inteligência artificial, desenvolvimento web, fintech, cloud computing
+                    e muito mais. Aprenda com especialistas e colegas em um ambiente colaborativo.
+                </x-slot>
+            </x-he4rt::card>
+
+            <x-he4rt::card class="col-span-5">
+                <x-slot:title>Vagas e Projetos Exclusivos</x-slot>
+                <x-slot:description>
+                    Empresas parceiras buscam talentos na comunidade 3 Pontos. Vagas de emprego, projetos freelancer e
+                    parcerias chegam primeiro para os membros. Sua próxima grande oportunidade pode estar a um clique de
+                    distância.
+                </x-slot>
+            </x-he4rt::card>
+        </div>
     </div>
 </section>
