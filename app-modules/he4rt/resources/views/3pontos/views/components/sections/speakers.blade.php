8a7b704240698d98fe94d232c9fe0cf5b0f43e19
kaster <diogokaster@gmail.com>
2025-11-17 18:09:40 -0300

    feat: add reusable heading component and update speakers section with enhanced card layout and styling

--- app-modules/he4rt/resources/views/3pontos/views/components/sections/speakers.blade.php
+++ app-modules/he4rt/resources/views/3pontos/views/components/sections/speakers.blade.php
@@ -9,12 +9,54 @@
             </x-he4rt::headline>
         </div>
 
-        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+        <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-8 lg:grid-cols-4">
             <x-he4rt::card :interactive="false" class="border-b-8">
                 <x-slot:header class="flex flex-col items-center justify-center border-none pb-0">
                     <div class="flex flex-col items-center gap-2">
                         <x-he4rt::avatar size="2xl" src="https://avatars.githubusercontent.com/u/85951158?v=4" />
-                        <h3 class="font-family-secondary tracking-tight lg:text-lg">Gabriel Vieira</h3>
+                        <x-he4rt::heading level="3" size="xs">Gabriel Vieira</x-he4rt::heading>
+                        <span class="text-text-medium text-sm">Product Designer</span>
+                    </div>
+                </x-slot>
+                <x-slot:description class="pb-4 text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae viverra nisi. Morbi at lorem
+                    facilisis enim eleifend sagittis at id massa.
+                </x-slot>
+            </x-he4rt::card>
+
+            <x-he4rt::card :interactive="false" class="border-b-8">
+                <x-slot:header class="flex flex-col items-center justify-center border-none pb-0">
+                    <div class="flex flex-col items-center gap-2">
+                        <x-he4rt::avatar size="2xl" src="https://avatars.githubusercontent.com/u/85951158?v=4" />
+                        <x-he4rt::heading level="3" size="xs">Gabriel Vieira</x-he4rt::heading>
+                        <span class="text-text-medium text-sm">Product Designer</span>
+                    </div>
+                </x-slot>
+                <x-slot:description class="pb-4 text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae viverra nisi. Morbi at lorem
+                    facilisis enim eleifend sagittis at id massa.
+                </x-slot>
+            </x-he4rt::card>
+
+            <x-he4rt::card :interactive="false" class="border-b-8">
+                <x-slot:header class="flex flex-col items-center justify-center border-none pb-0">
+                    <div class="flex flex-col items-center gap-2">
+                        <x-he4rt::avatar size="2xl" src="https://avatars.githubusercontent.com/u/85951158?v=4" />
+                        <x-he4rt::heading level="3" size="xs">Gabriel Vieira</x-he4rt::heading>
+                        <span class="text-text-medium text-sm">Product Designer</span>
+                    </div>
+                </x-slot>
+                <x-slot:description class="pb-4 text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae viverra nisi. Morbi at lorem
+                    facilisis enim eleifend sagittis at id massa.
+                </x-slot>
+            </x-he4rt::card>
+
+            <x-he4rt::card :interactive="false" class="border-b-8">
+                <x-slot:header class="flex flex-col items-center justify-center border-none pb-0">
+                    <div class="flex flex-col items-center gap-2">
+                        <x-he4rt::avatar size="2xl" src="https://avatars.githubusercontent.com/u/85951158?v=4" />
+                        <x-he4rt::heading level="3" size="xs">Gabriel Vieira</x-he4rt::heading>
                         <span class="text-text-medium text-sm">Product Designer</span>
                     </div>
                 </x-slot>
