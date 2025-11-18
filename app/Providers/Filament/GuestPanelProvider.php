2184d3bb291c9ed7037fe04c65ec997aeaa0a14f
danielhe4rt <danielhe4rt@gmail.com>
2025-11-18 17:38:28 -0300

    revert: panel name

--- app/Providers/Filament/GuestPanelProvider.php
+++ app/Providers/Filament/GuestPanelProvider.php
@@ -30,7 +30,7 @@ final class GuestPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('guest')
-            ->path('fodase')
+            ->path('')
             ->colors([
                 'primary' => Color::Purple,
             ])
