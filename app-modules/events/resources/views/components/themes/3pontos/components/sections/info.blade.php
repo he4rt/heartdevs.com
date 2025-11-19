ab40488545e21328fef369e6fcd9b1ef55dcb0a7
kaster <diogokaster@gmail.com>
2025-11-19 14:34:12 -0300

    feat: update info section layout in 3pontos theme with new map image, gradient overlay, and height adjustment

--- app-modules/events/resources/views/components/themes/3pontos/components/sections/info.blade.php
+++ app-modules/events/resources/views/components/themes/3pontos/components/sections/info.blade.php
@@ -1,7 +1,10 @@
 <section class="hp-section relative z-1">
     <div class="hp-container">
         <div class="grid grid-cols-1 gap-y-8 lg:grid-cols-2">
-            <div class="h-full w-full bg-red-500">map</div>
+            <div class="relative h-full w-full">
+                <img src="{{ asset('images/3pontos/3pontos-map.png') }}" alt="map" />
+                <div class="from-elevation-surface/10 absolute inset-0 bg-gradient-to-t to-transparent"></div>
+            </div>
             <div class="flex flex-col items-center justify-center lg:pl-16">
                 <x-he4rt::headline>
                     <x-slot:title>Informações sobre o evento</x-slot>
