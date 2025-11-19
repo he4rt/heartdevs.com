cf93f88f73b267ced7c767bc6c2d1e6aa00d4320
kaster <diogokaster@gmail.com>
2025-11-18 21:44:31 -0300

    feat: enhance about section with new SVG background and visual improvements in 3pontos theme

--- app-modules/events/resources/views/components/themes/3pontos/components/sections/about.blade.php
+++ app-modules/events/resources/views/components/themes/3pontos/components/sections/about.blade.php
@@ -1,9 +1,13 @@
-<section class="hp-section bg-elevation-01dp border-outline-dark min-h-0 border-t border-b">
-    <div class="hp-container">
+<section class="hp-section bg-elevation-01dp border-outline-dark relative min-h-0 overflow-hidden border-t border-b">
+    <div class="absolute right-0 bottom-0 z-1 h-1/2 w-1/2 translate-x-1/2 lg:-translate-y-1/2">
+        <img src="{{ asset('images/3pontos/3pontos-ball.svg') }}" alt="" class="h-auto w-full object-cover" />
+    </div>
+
+    <div class="hp-container relative z-10">
         <div>
             <x-he4rt::headline :keywords="['Lorem', 'ipsum']">
                 <x-slot:badge>
-                    <x-he4rt::section-title>Section name</x-he4rt::section-title>
+                    <x-3pontos::section-title>Section name</x-3pontos::section-title>
                 </x-slot>
                 <x-slot:title>Lorem ipsum um pouco da 3 pontos</x-slot>
                 <x-slot:description>
