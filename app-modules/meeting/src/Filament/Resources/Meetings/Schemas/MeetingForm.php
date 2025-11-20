a011943856107bb9eb7b0a3dbf9f704fb9ad0225
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-11-19 21:37:58 -0300

    chore(admin-panel): layout for meeting form

--- app-modules/meeting/src/Filament/Resources/Meetings/Schemas/MeetingForm.php
+++ app-modules/meeting/src/Filament/Resources/Meetings/Schemas/MeetingForm.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace He4rt\Meeting\Filament\Resources\Meetings\Schemas;
 
-use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\TimePicker;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class MeetingForm
@@ -17,40 +17,56 @@ class MeetingForm
     {
         return $schema
             ->components([
-                Select::make('tenant_id')
-                    ->relationship('tenant', 'name')
-                    ->required(),
-                Select::make('admin_id')
-                    ->relationship('admin', 'name')
-                    ->required(),
-                RichEditor::make('content')
-                    ->columnSpanFull(),
-                Select::make('meeting_type_id')
-                    ->relationship('meetingType', 'name')
-                    ->createOptionForm([
-                        TextInput::make('name')
-                            ->minLength(3)
-                            ->maxLength(255)
+                Section::make('Administration Area')
+                    ->description('Tenant and Administration')
+                    ->schema([
+                        Select::make('tenant_id')
+                            ->relationship('tenant', 'name')
                             ->required(),
-                        Select::make('week_day')
-                            ->options([
-                                1 => 'Monday',
-                                2 => 'Tuesday',
-                                3 => 'Wednesday',
-                                4 => 'Thursday',
-                                5 => 'Friday',
-                                6 => 'Saturday',
-                                7 => 'Sunday',
+                        Select::make('admin_id')
+                            ->relationship('admin', 'name')
+                            ->required(),
+                    ]),
+                Section::make('Meeting Data')
+                    ->description('Choose the Start and End')
+                    ->schema([
+                        TimePicker::make('starts_at')
+                            ->hoursStep(2)
+                            ->required(),
+                        TimePicker::make('ends_at')
+                            ->hoursStep(2)
+                            ->after('starts_at')
+                            ->required(),
+                    ]),
+                Section::make('Meeting Fields')
+                    ->description('Meeting Region')
+                    ->schema([
+                        RichEditor::make('content')
+                            ->columnSpanFull(),
+                        Select::make('meeting_type_id')
+                            ->relationship('meetingType', 'name')
+                            ->createOptionForm([
+                                TextInput::make('name')
+                                    ->minLength(3)
+                                    ->maxLength(255)
+                                    ->required(),
+                                Select::make('week_day')
+                                    ->options([
+                                        1 => 'Monday',
+                                        2 => 'Tuesday',
+                                        3 => 'Wednesday',
+                                        4 => 'Thursday',
+                                        5 => 'Friday',
+                                        6 => 'Saturday',
+                                        7 => 'Sunday',
+                                    ])
+                                    ->required(),
+                                TimePicker::make('start_at')
+                                    ->required(),
                             ])
                             ->required(),
-                        TimePicker::make('start_at')
-                            ->required(),
                     ])
-                    ->required(),
-                DateTimePicker::make('starts_at')
-                    ->required(),
-                DateTimePicker::make('ends_at')
-                    ->after('starts_at'),
+                    ->columnSpanFull(),
             ]);
     }
 }
