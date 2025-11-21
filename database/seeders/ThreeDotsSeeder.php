adc03710522784d04b0954254ec6d8309882229c
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2025-11-21 14:42:11 -0300

    feat(events): update event seeder to use withStatus for improved attendee management

--- database/seeders/ThreeDotsSeeder.php
+++ database/seeders/ThreeDotsSeeder.php
@@ -30,7 +30,7 @@ class ThreeDotsSeeder extends Seeder
             ]);
 
         $event = EventModel::factory()
-            ->withAttendees()
+            ->withStatus()
             ->state([
                 'title' => 'Evento Da Três Pontos',
                 'event_type' => EventTypeEnum::Workshop,
