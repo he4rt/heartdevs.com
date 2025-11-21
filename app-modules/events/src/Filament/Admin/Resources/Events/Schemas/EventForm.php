962c2b84f3959492396ab51a41cfa149bacc65c0
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-11-21 17:05:47 -0300

    wip

--- app-modules/events/src/Filament/Admin/Resources/Events/Schemas/EventForm.php
+++ app-modules/events/src/Filament/Admin/Resources/Events/Schemas/EventForm.php
@@ -12,7 +12,6 @@ use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
 use He4rt\Events\Enums\EventTypeEnum;
-use He4rt\Tenant\Filament\Admin\Resources\Tenants\RelationManagers\EventsRelationManager;
 use Illuminate\Support\Str;
 
 class EventForm
--- app-modules/events/tests/Feature/Filament/Admin/Event/CreateEventTest.php
+++ app-modules/events/tests/Feature/Filament/Admin/Event/CreateEventTest.php
@@ -96,7 +96,6 @@ describe('validation tests', function (): void {
             ->assertHasFormErrors(['event_type' => $rule]);
     })->with([
         'required' => ['', 'required'],
-        'enum' => ['aa', 'The selected event Type is invalid.'],
     ]);
 
     test('max_attendees::validations', function ($value, $rule): void {
