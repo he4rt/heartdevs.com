0e24e9e105181fff8dbb5baabce95642ed7ae685
kaster <diogokaster@gmail.com>
2025-11-27 11:17:55 -0300

    feat: enhance avatar fallback

--- app-modules/he4rt/resources/views/components/avatar.blade.php
+++ app-modules/he4rt/resources/views/components/avatar.blade.php
@@ -1,18 +1,24 @@
 @props([
     'circular' => true,
     'size' => 'md',
-    'src' => '',
+    'src' => null,
     'alt' => '',
 ])
 
-<img
-    {{
-        $attributes->class([
-            'hp-avatar',
-            'hp-circular' => $circular,
-            'hp-size-' . $size,
-        ])
-    }}
-    alt="{{ $alt }}"
-    src="{{ $src }}"
-/>
+@php
+    $classes = $attributes->class([
+        'hp-avatar',
+        'hp-circular' => $circular,
+        'hp-size-' . $size,
+    ]);
+@endphp
+
+@if ($src)
+    <img {{ $classes }} src="{{ $src }}" alt="{{ $alt }}" />
+@else
+    <div
+        {{ $classes->merge(['class' => 'flex items-center justify-center overflow-hidden bg-transparent text-text-medium']) }}
+    >
+        <x-heroicon-o-user-circle class="h-full w-full" />
+    </div>
+@endif
--- app-modules/he4rt/resources/views/components/ticket.blade.php
+++ app-modules/he4rt/resources/views/components/ticket.blade.php
@@ -1,5 +1,5 @@
 @props([
-    'userImg',
+    'userImg' => '',
     'username',
     'githubUsername',
     'ticketNumber',
