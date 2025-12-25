9a1228264dc66bc73bdcc5c85ab7245751e0515d
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2025-12-25 03:51:48 -0300

    feat(discord): add channel configuration for 100 dias de codigo

--- app-modules/bot-discord/config/bot-discord.php
+++ app-modules/bot-discord/config/bot-discord.php
@@ -6,6 +6,7 @@ return [
     'channels' => [
         'auto-report' => env('HE4RT_AUTO_REPORT_CHANNEL_ID', '1045804587195576451'),
         'presentations' => env('HE4RT_PRESENTATIONS_CHANNEL_ID', '540993663468306433'),
+        '100-dias-de-codigo' => env('HE4RT_100_DIAS_DE_CODIGO_CHANNEL_ID', '1140675099188543579'),
     ],
     'roles' => [
         'presentation' => env('HE4RT_PRESENTATION_ROLE_ID', '546150872397119491'),
