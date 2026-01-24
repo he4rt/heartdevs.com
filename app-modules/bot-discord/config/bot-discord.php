5d005c089b8e9ed291de6c202a16f3ad2fd8121e
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2026-01-24 03:12:04 -0300

    feat(discord): add technology roles for selection in bot configuration

--- app-modules/bot-discord/config/bot-discord.php
+++ app-modules/bot-discord/config/bot-discord.php
@@ -9,5 +9,25 @@ return [
     ],
     'roles' => [
         'presentation' => env('HE4RT_PRESENTATION_ROLE_ID', '546150872397119491'),
+
+        // Tecnologias e especialidades disponíveis para seleção
+        'technologies' => [
+            'basic_english' => '546148708077666315',
+            'intermediate_english' => '546148711416332298',
+            'advanced_english' => '546148712833875985',
+            'designer' => '547606728179449895',
+            'ux_ui' => '546152565633449995',
+            'javascript' => '540993488410378281',
+            'php' => '540994118634176512',
+            'python' => '540994295541399552',
+            'java' => '540995379538165774',
+            'c_c++_c#' => '541021498064896000',
+            'rust' => '1043325810347606056',
+            'ruby' => '540995627559944207',
+            'perl' => '540995072246939648',
+            'elixir' => '958788344974831687',
+            'gamedev' => '550411669084569602',
+            'he4rt_delas' => '1018009963903328308',
+        ],
     ],
 ];
