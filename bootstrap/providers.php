ad042e11db545aa2ea4dac091858ba49653eb361
danielhe4rt <danielhe4rt@gmail.com>
2026-05-01 19:55:02 -0300

    style: fix pint formatting in bootstrap/providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -12,6 +12,6 @@ return [
     AppServiceProvider::class,
     EventServiceProvider::class,
     FilamentServiceProvider::class,
-    RouteServiceProvider::class,
     AdminPanelProvider::class,
+    RouteServiceProvider::class,
 ];
