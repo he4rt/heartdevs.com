1ae28fef5f1b7e121abbe8ac97e969e70fd80dc0
danielhe4rt <danielhe4rt@gmail.com>
2026-05-01 19:44:54 -0300

    fix(panel-admin): use BackedEnum type for Dashboard navigationIcon
    Filament v5 changed $navigationIcon to accept string|BackedEnum|null.

--- app-modules/panel-admin/src/Pages/Dashboard.php
+++ app-modules/panel-admin/src/Pages/Dashboard.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace He4rt\PanelAdmin\Pages;
 
+use BackedEnum;
 use Filament\Pages\Dashboard as BaseDashboard;
 
 class Dashboard extends BaseDashboard
 {
-    protected static ?string $navigationIcon = 'heroicon-o-home';
+    protected static string|null|BackedEnum $navigationIcon = 'heroicon-o-home';
 
     protected static ?string $title = 'Dashboard';
 }
