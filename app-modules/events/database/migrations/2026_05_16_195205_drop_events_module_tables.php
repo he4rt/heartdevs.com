52a0bb9dff80b37bf890da1e8841e65959fb2556
danielhe4rt <danielhe4rt@gmail.com>
2026-05-16 20:01:47 -0300

    remove: legacy events system

---
+++ app-modules/events/database/migrations/2026_05_16_195205_drop_events_module_tables.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::dropIfExists('event_submission_speakers');
+        Schema::dropIfExists('events_agenda');
+        Schema::dropIfExists('events_sponsors');
+        Schema::dropIfExists('events_attendees');
+        Schema::dropIfExists('events_talks');
+        Schema::dropIfExists('sponsors');
+        Schema::dropIfExists('events');
+    }
+};
