8f99fa9bd73903a8495be9a4d40bdecc1e6c366e
danielhe4rt <danielhe4rt@gmail.com>
2026-05-19 21:13:33 -0300

    feat(integration-discord): wire up observer and sync command in service provider

--- app-modules/integration-discord/src/IntegrationDiscordServiceProvider.php
+++ app-modules/integration-discord/src/IntegrationDiscordServiceProvider.php
@@ -7,6 +7,9 @@ namespace He4rt\IntegrationDiscord;
 use He4rt\IntegrationDiscord\ETL\Console\ImportDiscordMessagesCommand;
 use He4rt\IntegrationDiscord\ETL\Console\ImportDiscordProfilesCommand;
 use He4rt\IntegrationDiscord\ETL\Console\MergeDuplicateDiscordProfilesCommand;
+use He4rt\IntegrationDiscord\Models\DiscordEventLog;
+use He4rt\IntegrationDiscord\Sync\Console\SyncDiscordGuildCommand;
+use He4rt\IntegrationDiscord\Sync\Observers\DiscordEventLogObserver;
 use He4rt\IntegrationDiscord\Transport\DiscordConnector;
 use He4rt\IntegrationDiscord\Transport\DiscordOAuthConnector;
 use Illuminate\Support\ServiceProvider;
@@ -28,11 +31,14 @@ class IntegrationDiscordServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        DiscordEventLog::observe(DiscordEventLogObserver::class);
+
         if ($this->app->runningInConsole()) {
             $this->commands([
                 ImportDiscordProfilesCommand::class,
                 ImportDiscordMessagesCommand::class,
                 MergeDuplicateDiscordProfilesCommand::class,
+                SyncDiscordGuildCommand::class,
             ]);
         }
     }
