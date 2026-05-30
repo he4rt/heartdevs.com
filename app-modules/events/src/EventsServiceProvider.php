4e2a0a3240dc575060a7096afe7d639f6557b2dc
YuriSouzaDev <yurisouza.developer@gmail.com>
2026-05-29 23:16:12 -0300

    feat(events): registra listener GenerateQrTokenOnConfirmed no EventsServiceProvider
    Conecta EnrollmentConfirmed → GenerateQrTokenOnConfirmed para geração automática
    do token QR quando a inscrição é confirmada.

--- app-modules/events/src/EventsServiceProvider.php
+++ app-modules/events/src/EventsServiceProvider.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace He4rt\Events;
 
+use He4rt\Events\CheckIn\Listeners\GenerateQrTokenOnConfirmed;
+use He4rt\Events\Enrollment\Events\EnrollmentConfirmed;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class EventsServiceProvider extends ServiceProvider
@@ -13,5 +16,7 @@ class EventsServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'events');
         $this->loadTranslationsFrom(__DIR__.'/../lang', 'events');
+
+        Event::listen(EnrollmentConfirmed::class, GenerateQrTokenOnConfirmed::class);
     }
 }
