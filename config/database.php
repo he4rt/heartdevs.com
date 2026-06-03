e0837e4710e2270f805ab30aaceb1f8b1862b8ea
davi.oliveira <davi.oliveira@ernestoborges.com.br>
2026-06-03 10:54:50 -0400

    fix(database): removing UTC as global timezone in database

--- config/database.php
+++ config/database.php
@@ -99,7 +99,6 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
-            'timezone' => 'UTC',
         ],
 
         'sqlsrv' => [
