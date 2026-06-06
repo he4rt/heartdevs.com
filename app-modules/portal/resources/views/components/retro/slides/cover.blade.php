d85c602d20b298b11ce2add5494dde485ed55914
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2026-06-06 10:24:43 -0300

    feat(portal): anima a linha do batimento (ECG) com pulso contínuo na capa

--- app-modules/portal/resources/views/components/retro/slides/cover.blade.php
+++ app-modules/portal/resources/views/components/retro/slides/cover.blade.php
@@ -9,7 +9,15 @@
         </div>
         <h1 class="hero" data-anim>Quem fez a He4rt <em>bater</em></h1>
         <svg class="ecg" viewBox="0 0 560 60" preserveAspectRatio="none" data-anim aria-hidden="true">
-            <path d="M0 34 H210 l10 0 l8 -7 l11 14 l13 -34 l15 50 l11 -22 l9 0 H340 l9 0 l8 -6 l9 12 l12 -28 l13 40 l10 -16 l8 0 H560" />
+            <path
+                class="trace"
+                d="M0 34 H210 l10 0 l8 -7 l11 14 l13 -34 l15 50 l11 -22 l9 0 H340 l9 0 l8 -6 l9 12 l12 -28 l13 40 l10 -16 l8 0 H560"
+            />
+            <path
+                class="pulse"
+                pathLength="100"
+                d="M0 34 H210 l10 0 l8 -7 l11 14 l13 -34 l15 50 l11 -22 l9 0 H340 l9 0 l8 -6 l9 12 l12 -28 l13 40 l10 -16 l8 0 H560"
+            />
         </svg>
         <p
             class="lead"
