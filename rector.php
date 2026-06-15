6323c1b01a1e276ac78aa880993e42e7c23337d0
Gabriel do Carmo Vieira <48625433+gvieira18@users.noreply.github.com>
2026-06-14 23:20:32 -0300

    chore(tooling): update rector and pint rules
    Add StaticClosureRector, EnsureTypeChecksFirstRector,
    Laravel 13 attribute rectors, Pest 4.0 set, and Pint
    numeric_literal_separator rule.

--- rector.php
+++ rector.php
@@ -3,22 +3,61 @@
 declare(strict_types=1);
 
 use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
 use Rector\CodingStyle\Rector\PostInc\PostIncDecToPreIncDecRector;
 use Rector\Config\RectorConfig;
 use Rector\Php70\Rector\StaticCall\StaticCallOnNonStaticToInstanceCallRector;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 use Rector\TypeDeclaration\Rector\ArrowFunction\AddArrowFunctionReturnTypeRector;
 use RectorLaravel\Rector\Class_\AddHasFactoryToModelsRector;
-use RectorLaravel\Rector\Class_\FillablePropertyToFillableAttributeRector;
+use RectorLaravel\Rector\Class_\BackoffPropertyToBackoffAttributeRector;
+use RectorLaravel\Rector\Class_\ConnectionPropertyToConnectionAttributeRector;
+use RectorLaravel\Rector\Class_\DelayPropertyToDelayAttributeRector;
+use RectorLaravel\Rector\Class_\DeleteWhenMissingModelsPropertyToDeleteWhenMissingModelsAttributeRector;
+use RectorLaravel\Rector\Class_\FailOnTimeoutPropertyToFailOnTimeoutAttributeRector;
+use RectorLaravel\Rector\Class_\JobConnectionPropertyToJobConnectionAttributeRector;
+use RectorLaravel\Rector\Class_\MaxExceptionsPropertyToMaxExceptionsAttributeRector;
 use RectorLaravel\Rector\Class_\ModelCastsPropertyToCastsMethodRector;
+use RectorLaravel\Rector\Class_\QueuePropertyToQueueAttributeRector;
 use RectorLaravel\Rector\Class_\ReplaceExpectsMethodsInTestsRector;
+use RectorLaravel\Rector\Class_\StopOnFirstFailurePropertyToStopOnFirstFailureAttributeRector;
+use RectorLaravel\Rector\Class_\TablePropertyToTableAttributeRector;
+use RectorLaravel\Rector\Class_\TimeoutPropertyToTimeoutAttributeRector;
+use RectorLaravel\Rector\Class_\TriesPropertyToTriesAttributeRector;
+use RectorLaravel\Rector\Class_\UniqueForPropertyToUniqueForAttributeRector;
+use RectorLaravel\Rector\Class_\WithoutIncrementingPropertyToWithoutIncrementingAttributeRector;
+use RectorLaravel\Rector\Class_\WithoutTimestampsPropertyToWithoutTimestampsAttributeRector;
 use RectorLaravel\Rector\Coalesce\ApplyDefaultInsteadOfNullCoalesceRector;
 use RectorLaravel\Rector\Empty_\EmptyToBlankAndFilledFuncRector;
 use RectorLaravel\Rector\FuncCall\ConfigToTypedConfigMethodCallRector;
 use RectorLaravel\Rector\MethodCall\RefactorBlueprintGeometryColumnsRector;
 use RectorLaravel\Rector\PropertyFetch\ReplaceFakerInstanceWithHelperRector;
 use RectorLaravel\Set\LaravelSetList;
+use RectorPest\Rules\EnsureTypeChecksFirstRector;
 use RectorPest\Set\PestSetList;
+
+$laravel13Attributes = [
+    // Eloquent
+    TablePropertyToTableAttributeRector::class,
+    ConnectionPropertyToConnectionAttributeRector::class,
+    WithoutIncrementingPropertyToWithoutIncrementingAttributeRector::class,
+    WithoutTimestampsPropertyToWithoutTimestampsAttributeRector::class,
+
+    // Form Request
+    StopOnFirstFailurePropertyToStopOnFirstFailureAttributeRector::class,
+
+    // Queue
+    TriesPropertyToTriesAttributeRector::class,
+    TimeoutPropertyToTimeoutAttributeRector::class,
+    BackoffPropertyToBackoffAttributeRector::class,
+    MaxExceptionsPropertyToMaxExceptionsAttributeRector::class,
+    JobConnectionPropertyToJobConnectionAttributeRector::class,
+    QueuePropertyToQueueAttributeRector::class,
+    DelayPropertyToDelayAttributeRector::class,
+    DeleteWhenMissingModelsPropertyToDeleteWhenMissingModelsAttributeRector::class,
+    FailOnTimeoutPropertyToFailOnTimeoutAttributeRector::class,
+    UniqueForPropertyToUniqueForAttributeRector::class,
+];
 
 return RectorConfig::configure()
     ->withPaths([
@@ -38,7 +77,6 @@ return RectorConfig::configure()
         AddArrowFunctionReturnTypeRector::class,
         AddHasFactoryToModelsRector::class,
         AddOverrideAttributeToOverriddenMethodsRector::class,
-        FillablePropertyToFillableAttributeRector::class,
         PostIncDecToPreIncDecRector::class,
         StaticCallOnNonStaticToInstanceCallRector::class,
         __DIR__.'/bootstrap/cache',
@@ -70,6 +108,9 @@ return RectorConfig::configure()
         ReplaceExpectsMethodsInTestsRector::class,
         ReplaceFakerInstanceWithHelperRector::class,
         ConfigToTypedConfigMethodCallRector::class,
+        EnsureTypeChecksFirstRector::class,
+        StaticClosureRector::class,
+        ...$laravel13Attributes,
     ])
     ->withSets([
         LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
@@ -85,5 +126,6 @@ return RectorConfig::configure()
         LaravelSetList::LARAVEL_TYPE_DECLARATIONS,
         PestSetList::PEST_CODE_QUALITY,
         PestSetList::PEST_LARAVEL,
-        LaravelSetList::LARAVEL_130,
+        PestSetList::PEST_40,
+        LaravelSetList::LARAVEL_130_WITHOUT_ATTRIBUTES,
     ]);
